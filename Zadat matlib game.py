e8b41a292eae6eaaaed6feaa3f1680d047c443ff
DemirSacirovic <121728128+DemirSacirovic@users.noreply.github.com>
2023-01-09 19:12:04 +0100

    Add files via upload

---
+++ Zadat matlib game.py
@@ -0,0 +1,10 @@
+boja = input("Unesite boju:")
+plural_noun = input("Unesite recenicu:")
+celebrity= input("Uneste celebrity:")
+
+print("Roses are " +boja)
+print(plural_noun + " are blue")
+print("I love" + celebrity)
+
+
+
